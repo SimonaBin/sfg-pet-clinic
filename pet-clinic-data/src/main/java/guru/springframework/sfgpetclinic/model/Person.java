0aa838f797434719912be60a6c8a1a5a11f0f9c4
Simona Bineata <simona.bineata@metrosystems.net>
2022-01-15 14:30:35 +0200

    add Base Entity. Closes #10.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -1,11 +1,9 @@
 package guru.springframework.sfgpetclinic.model;
 
-public class Person {
+public class Person extends  BaseEntity{
 
     private String firstName;
     private String lastName;
-
-
 
     public String getFirstName() {
         return firstName;
